282eae047a9477bb538885e6605437919996013c
Michaela Lehr <michaela.lehr@geildanke.com>
2017-12-16 20:37:27 +0100

    ADD close button

--- components/play-video.js
+++ components/play-video.js
@@ -7,20 +7,25 @@
 AFRAME.registerComponent('play-video', {
   schema: {
     on: {type: 'string'},
-    url: {type: 'string'}
+    video: {type: 'string'}
   },
 
   init: function () {
     var el = this.el;
     var data = this.data;
-    var url = '<iframe width="560" height="315" src="' + data.url + '" frameborder="0" gesture="media" allow="encrypted-media" id="video" allowfullscreen></iframe>';
+    var url = '<iframe width="560" height="315" src="' + data.video + '" frameborder="0" gesture="media" allow="encrypted-media" id="video" allowfullscreen></iframe>';
     var video;
 
 
     el.addEventListener(data.on, function () {
       var div = document.createElement('div');
+      var button = document.createElement('button');
+
       document.body.appendChild(div).innerHTML = url;
+      div.appendChild(button);
+
       div.style = 'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);';
+      button.style = 'position: absolute; top: -40px; right: -40px; width: 40px; height: 40px; background-image: url("../assets/button_close.png"); background-size: cover; cursor: pointer; border: none;';
 
       video = document.getElementById('video').src += "?rel=0&autoplay=1";
     });
